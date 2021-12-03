790faadb73cfa24ee59ebdeb071c01a5b76b209d
Glakho Robakidze <glakhorobakidze@gmail.com>
2021-12-03 12:01:35 +0000

    fixed customer update

--- src/main/java/com/qa/ims/controller/CustomerController.java
+++ src/main/java/com/qa/ims/controller/CustomerController.java
@@ -80,6 +80,7 @@ public class CustomerController implements CrudController<Customer> {
 	 */
 	@Override
 	public Customer update() {
+		LOGGER.info("Please enter the id of the customer you would like to update");
 		LOGGER.info("Please enter a first name");
 		String firstName = utils.getString();
 		LOGGER.info("Please enter a surname");
